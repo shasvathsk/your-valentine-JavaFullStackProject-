00060e726b1032e0d01965eb47dd2564c7fe90f3
Yuvanesh-M-003 <yuvanmuthusamy333@gmail.com>
2026-02-23 11:19:21 +0530

    final commit

--- Valentine/src/main/java/com/example/Valentine/Controller/SpaForwardController.java
+++ Valentine/src/main/java/com/example/Valentine/Controller/SpaForwardController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SpaForwardController {
 
-    @GetMapping("/countdown/{id}")
+    @GetMapping(value = { "/", "/{path:[^\\.]*}" })
     public String forward() {
         return "forward:/index.html";
     }
